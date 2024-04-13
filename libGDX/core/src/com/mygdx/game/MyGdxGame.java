fc9cc9be5f1c5f163f418c28d21070b2c240a709
candace.govender <candace@psybergate.co.za>
2024-04-13 10:54:22 +0200

    new ui branch

--- libGDX/core/src/com/mygdx/game/MyGdxGame.java
+++ libGDX/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,36 +1,31 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
 
-  SpriteBatch batch;
-
-  ChessBoardRenderer chessBoard;
-
-  @Override
-  public void create() {
-    chessBoard = new ChessBoardRenderer();
-    chessBoard.create();
-  }
-
-  @Override
-  public void render() {
-    Gdx.gl.glClearColor(0, 0, 0, 1);
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-    chessBoard.render();
-    //summoningCircle.render();
-  }
-
-  @Override
-  public void dispose() {
-    chessBoard.dispose();
-    //    batch.dispose();
-    //summoningCircle.dispose();
-    //img.dispose();
-  }
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
 }
