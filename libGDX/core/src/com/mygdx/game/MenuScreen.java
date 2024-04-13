df13e7a884e880a94e8298c3e5142eb52f9de01c
candace.govender <candace@psybergate.co.za>
2024-04-13 10:55:29 +0200

    new ui branch

---
+++ libGDX/core/src/com/mygdx/game/MenuScreen.java
@@ -0,0 +1,39 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class MenuScreen implements Screen {
+  //add a stage and add actors to the stage
+  //Eventually you need to add Listeners to your button actors to switch screens
+
+  @Override
+  public void show() {
+
+  }
+
+  @Override
+  public void render(float v) {
+  }
+
+  @Override
+  public void resize(int i, int i1) {
+  }
+
+  @Override
+  public void pause() {
+  }
+
+  @Override
+  public void resume() {
+  }
+
+  @Override
+  public void hide() {
+  }
+
+  @Override
+  public void dispose() {
+  }
+}
