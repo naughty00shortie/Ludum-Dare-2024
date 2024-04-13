b0c7b2a73d1de4d46a92937f86b553de802146e9
candace.govender <candace@psybergate.co.za>
2024-04-13 12:42:20 +0200

    created Pawn class with moveset method complete

--- libGDX/core/src/com/mygdx/game/movement/Pawn.java
+++ libGDX/core/src/com/mygdx/game/movement/Pawn.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Pawn implements Piece{
+public class Pawn implements Piece {
 
   @Override
   public boolean isPlayerPiece() {
@@ -12,12 +12,24 @@ public class Pawn implements Piece{
   }
 
   @Override
-  public Set<CoOrdinatePair> moveSet(int xOrigin, int yOrigin) {
-    //basic movement moves 1 forward "up"
+  public Set<CoOrdinatePair> moveSet(int xOrigin, int yOrigin, Cell[][] board) {
+    CoOrdinatePair origin = new CoOrdinatePair(xOrigin, yOrigin);
     Set<CoOrdinatePair> possibleMoves = new HashSet<>();
-    possibleMoves.add(new CoOrdinatePair(xOrigin, yOrigin + 1));// todo assumed up is positive direction
-    //todo
-
-    return Collections.emptySet();
+    //todo if 1st move can move 2 spaces forward
+    //basic movement moves 1 forward "up"
+    CoOrdinatePair oneForward = new CoOrdinatePair(xOrigin, yOrigin + 1);
+    possibleMoves.add(oneForward);// todo assumed up is positive direction
+    //todo check if there are valid capture moves
+    //get diagonal cells
+    Cell cellLeftDiagonal = board[xOrigin - 1][yOrigin + 1];
+    Cell cellRightDiagonal = board[xOrigin + 1][yOrigin + 1];
+    //check if theres a piece
+    if (cellLeftDiagonal.isOccupied()) {
+      possibleMoves.add(new CoOrdinatePair(xOrigin - 1, yOrigin + 1));
+    }
+    if (cellRightDiagonal.isOccupied()) {
+      possibleMoves.add(new CoOrdinatePair(xOrigin + 1, yOrigin + 1));
+    }
+    return possibleMoves;
   }
 }
