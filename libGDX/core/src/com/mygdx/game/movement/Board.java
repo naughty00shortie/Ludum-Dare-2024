0da1bcd44ee8cfe134158d9c93deab2376c66386
iandro.vanderlinde <iandro@psybergate.co.za>
2024-04-13 12:57:43 +0200

    added cell navigation utils

--- libGDX/core/src/com/mygdx/game/movement/Board.java
+++ libGDX/core/src/com/mygdx/game/movement/Board.java
@@ -1,8 +1,12 @@
 package com.mygdx.game.movement;
 
+import com.mygdx.game.utils.CellNavigationUtils;
+
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+
+import static com.mygdx.game.utils.CellNavigationUtils.*;
 
 /**
  * Top level abstraction. The board is the source of most queries from the GUI.
@@ -10,102 +14,104 @@ import java.util.Set;
  */
 public class Board {
 
-  public static final int BOARD_SIZE = 8;
+    public static final int BOARD_SIZE = 8;
 
-  private Cell[][] cells = new Cell[BOARD_SIZE][BOARD_SIZE];
+    private Cell[][] cells = new Cell[BOARD_SIZE][BOARD_SIZE];
 
-  private void buildBoard() {
+    private void buildBoard() {
 
-  }
-
-  public Cell[][] getCells() {
-    return cells;
-  }
-
-  public void setCells(Cell[][] board) {
-    this.cells = board;
-  }
-
-  public Set<CoOrdinatePair> getValidMoves(int x, int y) {
-    Piece p = cells[x][y].getPiece().orElseThrow(() -> new RuntimeException());
-    return p.moveSet(x, y);
-  }
-
-  public void movePiece(Piece piece, int oldX, int oldY, int newX, int newY) {
-    // TODO bounds checking
-    // summon piece
-    // move piece, no piece at new position, we good
-    // move piece and there is an enemy piece, capture
-    Piece p = cells[oldX][oldY].removePiece();
-    cells[newX][newY].placePiece(p);
-  }
-
-  public void summon(Piece piece, int x, int y) {
-    cells[x][y].placePiece(piece);
-  }
-
-  public Set<Cell> getCellsWithPieces() {
-    Set<Cell> pieces = new HashSet<>();
-    for (int i = 0; i < cells.length; i++) {
-      for (int j = 0; j < cells[i].length; j++) {
-        Cell cell = cells[i][j];
-        if (cell.isOccupied()) pieces.add(cell);
-      }
     }
-    return pieces;
-  }
 
-
-  public Set<Cell> getSummonableCells() {
-    Set<Cell> summonablePositions = new HashSet<>();
-    Set<Cell> pieces = getCellsWithPieces();
-    for (int i = 0; i < cells.length; i++) {
-      for (int j = 0; j < cells[i].length; j++) {
-        Cell cell = cells[i][j];
-        if (!cell.isOccupied() && cellIsAdjacentToPiece(cell, pieces)) summonablePositions.add(cell);
-        if(j == 0) summonablePositions.add(cell);
-      }
+    public Cell[][] getCells() {
+        return cells;
     }
-    return summonablePositions;
-  }
 
-  // IANDRO: "this is nasty, so fix it if it doesn't work"
-  private boolean cellIsAdjacentToPiece(Cell cell, Set<Cell> pieces) {
-    int cellX = cell.getX();
-    int cellY = cell.getY();
-    for (Cell piece : pieces) {
-      if (piece.getX() + 1 == cellX && piece.getY() + 1 == cellY) return true; // top right diag
-      if (piece.getX() - 1 == cellX && piece.getY() - 1 == cellY) return true; // bottom left diag
-      if (piece.getX() + 1 == cellX && piece.getY() - 1 == cellY) return true; // bottom right
-      // diag
-      if (piece.getX() - 1 == cellX && piece.getY() + 1 == cellY) return true; // top left diag
-      if(piece.getX() == cellX && (piece.getY() + 1 == cellY || piece.getY() - 1 == cellY)) return true; // up or down one
-      if(piece.getY() == cellY && (piece.getX() + 1 == cellX || piece.getX() - 1 == cellX)) return true; // left or right one
+    public void setCells(Cell[][] board) {
+        this.cells = board;
     }
-    return false;
-  }
 
-
-  /**
-   * Get the Cell at this CoOrdinate
-   *
-   * @param coOrdinatePair to retrieve a Cell from.
-   * @return Cell if CoOrdinate is within the bounds of the Board.
-   */
-  public Optional<Cell> getCell(CoOrdinatePair coOrdinatePair) {
-    if (isInBounds(coOrdinatePair)) {
-      return Optional.of(cells[coOrdinatePair.getX()][coOrdinatePair.getY()]);
-    } else {
-      return Optional.empty();
+    public Set<CoOrdinatePair> getValidMoves(int x, int y) {
+        Piece p = cells[x][y].getPiece().orElseThrow(() -> new RuntimeException());
+        return p.moveSet(x, y);
     }
-  }
 
-  private boolean isInBounds(CoOrdinatePair coOrdinatePair) {
-    int x = coOrdinatePair.getX();
-    int y = coOrdinatePair.getY();
-    if (x < 0 || x >= BOARD_SIZE) return false;
-    if (y < 0 || y >= BOARD_SIZE) return false;
-    return true;
-  }
+    public void movePiece(Piece piece, int oldX, int oldY, int newX, int newY) {
+        // TODO bounds checking
+        // summon piece
+        // move piece, no piece at new position, we good
+        // move piece and there is an enemy piece, capture
+        Piece p = cells[oldX][oldY].removePiece();
+        cells[newX][newY].placePiece(p);
+    }
+
+    public void summon(Piece piece, int x, int y) {
+        cells[x][y].placePiece(piece);
+    }
+
+    public Set<Cell> getCellsWithPieces() {
+        Set<Cell> pieces = new HashSet<>();
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                Cell cell = cells[i][j];
+                if (cell.isOccupied()) pieces.add(cell);
+            }
+        }
+        return pieces;
+    }
+
+
+    public Set<Cell> getSummonableCells() {
+        Set<Cell> summonablePositions = new HashSet<>();
+        Set<Cell> pieces = getCellsWithPieces();
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                Cell cell = cells[i][j];
+                if (! cell.isOccupied() && cellIsAdjacentToPiece(cell, pieces))
+                    summonablePositions.add(cell);
+                if (j == 0) summonablePositions.add(cell);
+            }
+        }
+        return summonablePositions;
+    }
+
+    // IANDRO: "this is nasty, so fix it if it doesn't work"
+    private boolean cellIsAdjacentToPiece(Cell cell, Set<Cell> pieces) {
+        int cellX = cell.getX();
+        int cellY = cell.getY();
+        for (Cell piece : pieces) {
+            if (moveOneCellDiagonalTopRight(piece).equals(cell)) return true;
+            if (moveOneCellDiagonalBottomRight(piece).equals(cell)) return true;
+            if (moveOneCellDiagonalBottomLeft(piece).equals(cell)) return true;
+            if (moveOneCellDiagonalTopLeft(piece).equals(cell)) return true;
+            if (moveOneCellLeft(piece).equals(cell)) return true;
+            if (moveOneCellRight(piece).equals(cell)) return true;
+            if (moveOneCellUp(piece).equals(cell)) return true;
+            if (moveOneCellDown(piece).equals(cell)) return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Get the Cell at this CoOrdinate
+     *
+     * @param coOrdinatePair to retrieve a Cell from.
+     * @return Cell if CoOrdinate is within the bounds of the Board.
+     */
+    public Optional<Cell> getCell(CoOrdinatePair coOrdinatePair) {
+        if (isInBounds(coOrdinatePair)) {
+            return Optional.of(cells[coOrdinatePair.getX()][coOrdinatePair.getY()]);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private boolean isInBounds(CoOrdinatePair coOrdinatePair) {
+        int x = coOrdinatePair.getX();
+        int y = coOrdinatePair.getY();
+        if (x < 0 || x >= BOARD_SIZE) return false;
+        if (y < 0 || y >= BOARD_SIZE) return false;
+        return true;
+    }
 
 }
---
+++ libGDX/core/src/com/mygdx/game/utils/CellNavigationUtils.java
@@ -0,0 +1,88 @@
+package com.mygdx.game.utils;
+
+import com.mygdx.game.movement.Cell;
+import com.mygdx.game.movement.CoOrdinatePair;
+
+public class CellNavigationUtils {
+
+    public static CoOrdinatePair moveOneCellUp(int x, int y) {
+        return new CoOrdinatePair(x, y + 1);
+    }
+
+    public static CoOrdinatePair moveOneCellDown(int x, int y) {
+        return new CoOrdinatePair(x, y - 1);
+    }
+
+    public static CoOrdinatePair moveOneCellLeft(int x, int y) {
+        return new CoOrdinatePair(x - 1, y);
+    }
+
+    public static CoOrdinatePair moveOneCellRight(int x, int y) {
+        return new CoOrdinatePair(x + 1, y);
+    }
+
+    public static CoOrdinatePair moveOneCellDiagonalTopLeft(int x, int y) {
+        return new CoOrdinatePair(x - 1, y + 1);
+    }
+
+    public static CoOrdinatePair moveOneCellDiagonalBottomLeft(int x, int y) {
+        return new CoOrdinatePair(x - 1, y - 1);
+    }
+
+    public static CoOrdinatePair moveOneCellDiagonalBottomRight(int x, int y) {
+        return new CoOrdinatePair(x + 1, y - 1);
+    }
+
+    public static CoOrdinatePair moveOneCellDiagonalTopRight(int x, int y) {
+        return new CoOrdinatePair(x + 1, y + 1);
+    }
+
+    public static Cell moveOneCellUp(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+        return new Cell(x, y + 1, cell.getRectangle());
+    }
+
+    public static Cell moveOneCellDown(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+        return new Cell(x, y - 1, cell.getRectangle());
+    }
+
+    public static Cell moveOneCellLeft(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+        return new Cell(x - 1, y, cell.getRectangle());
+    }
+
+    public static Cell moveOneCellRight(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+        return new Cell(x + 1, y, cell.getRectangle());
+    }
+
+    public static Cell moveOneCellDiagonalTopLeft(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+        return new Cell(x - 1, y + 1, cell.getRectangle());
+    }
+
+    public static Cell moveOneCellDiagonalBottomLeft(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+        return new Cell(x - 1, y - 1, cell.getRectangle());
+    }
+
+    public static Cell moveOneCellDiagonalBottomRight(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+        return new Cell(x + 1, y - 1, cell.getRectangle());
+    }
+
+    public static Cell moveOneCellDiagonalTopRight(Cell cell) {
+        int x = cell.getX();
+        int y = cell.getY();
+        return new Cell(x + 1, y + 1, cell.getRectangle());
+    }
+
+}
