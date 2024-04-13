8b816d284ed69c3c7a2a7c7f8edf2fee3f3297ff
riece <riece@psybergate.co.za>
2024-04-13 17:03:41 +0200

    Piece abs refactor, rm Castle, Knight, Pawn.

--- libGDX/core/src/com/mygdx/game/movement/pieces/Grunt.java
+++ libGDX/core/src/com/mygdx/game/movement/pieces/Grunt.java
@@ -3,9 +3,7 @@ package com.mygdx.game.movement.pieces;
 import com.mygdx.game.movement.CoOrdinatePair;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Basic Summon of the game. Marches and captures in one direction.
@@ -27,38 +25,20 @@ public class Grunt implements Piece {
     return isPlayerPiece;
   }
 
-
   /**
    * Player Pieces move up the Board, increasing in Y, whereas
    * Enemy Pieces move down the Board, decreasing in Y.
    */
   @Override
-  public Set<CoOrdinatePair> moveSet(int xOrigin, int yOrigin, Board board) {
-    return potentialPositions(xOrigin, yOrigin).stream()
-            .map(board::getCell)
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .filter(this::canOccupyCell)
-            .map(Cell::getCoOrdinatePair)
-            .collect(Collectors.toSet());
-  }
-
-  /**
-   * @return all potential positions the Grunt could move to.
-   */
-  private Set<CoOrdinatePair> potentialPositions(int xOrigin, int yOrigin) {
-  public Set<CoOrdinatePair> potentialMoveSet(CoOrdinatePair origin) {
-    CoOrdinatePair step = (isPlayerPiece()) ? origin.up() : origin.down();
+  public Set<CoOrdinatePair> potentialMoveSet(int xOrigin, int yOrigin) {
+    int yStep = (isPlayerPiece()) ? yOrigin + 1 : yOrigin - 1;
     Set<CoOrdinatePair> moveSet = new HashSet<>();
-    CoOrdinatePair forward = new CoOrdinatePair(xOrigin, yOrigin + 1);
-    CoOrdinatePair left = new CoOrdinatePair(xOrigin + 1, yOrigin + 1);
-    CoOrdinatePair right = new CoOrdinatePair(xOrigin - 1, yOrigin - 1);
+    CoOrdinatePair forward = new CoOrdinatePair(xOrigin, yStep);
+    CoOrdinatePair left = new CoOrdinatePair(xOrigin + 1, yStep);
+    CoOrdinatePair right = new CoOrdinatePair(xOrigin - 1, yStep);
     moveSet.add(forward);
     moveSet.add(left);
     moveSet.add(right);
-    moveSet.add(step);
-    moveSet.add(step.left());
-    moveSet.add(step.right());
     return moveSet;
   }
 
