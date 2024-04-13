77c39e6694021715f3d8fba572219d140ef6f4a5
candace.govender <candace@psybergate.co.za>
2024-04-13 10:54:22 +0200

    new ui branch

--- libGDX/core/src/com/mygdx/game/MyGdxGame.java
+++ libGDX/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,26 +1,31 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
 
-  ChessBoardRenderer chessBoardRenderer;
-
-  @Override
-  public void create() {
-    chessBoardRenderer = new ChessBoardRenderer();
-    chessBoardRenderer.create();
-  }
-
-  @Override
-  public void render() {
-    ScreenUtils.clear(1, 0, 0, 1);
-    chessBoardRenderer.render();
-  }
-
-  @Override
-  public void dispose() {
-    chessBoardRenderer.dispose();
-  }
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
 }
