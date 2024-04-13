daa69ae17aa1004a5508ee94d16cb75399255831
Albertus Van der Walt <naughty00shortie@gmail.com>
2024-04-13 11:12:35 +0200

    implemented basic example

--- libGDX/core/src/com/mygdx/game/MyGdxGame.java
+++ libGDX/core/src/com/mygdx/game/MyGdxGame.java
@@ -3,29 +3,25 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+  private Stage stage;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+  private Skin skin;
+
+  @Override
+  public void create() {
+    skin = new Skin();
+  }
+
+  @Override
+  public void render() {
+  }
+
+  @Override
+  public void dispose() {
+  }
 }
