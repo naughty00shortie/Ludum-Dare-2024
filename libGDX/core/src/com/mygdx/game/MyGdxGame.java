63711d92cb0e3e885a1150727d259ac45cb62f02
candace.govender <candace@psybergate.co.za>
2024-04-14 13:26:37 +0200

    SoundUtils class done. add more sounds to the map

--- libGDX/core/src/com/mygdx/game/MyGdxGame.java
+++ libGDX/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.mygdx.game.ui.ChessBoardRenderer;
 import com.mygdx.game.ui.ManaUI;
 import com.mygdx.game.ui.MenuSummonScreen;
@@ -13,16 +15,16 @@ public class MyGdxGame extends ApplicationAdapter {
 
   @Override
   public void create() {
-
-     menuScreen = new MenuSummonScreen();
-     menuScreen.create();
+    menuScreen = new MenuSummonScreen();
+    menuScreen.create();
     //chessBoardRenderer = new ChessBoardRenderer();
     //chessBoardRenderer.create();
+    //todo add this to main game class to enable sound
+    SoundUtils.create();
   }
 
   @Override
   public void render() {
-
     //chessBoardRenderer.render();
     menuScreen.render();
   }
@@ -31,5 +33,7 @@ public class MyGdxGame extends ApplicationAdapter {
   public void dispose() {
     menuScreen.dispose();
     //chessBoardRenderer.dispose();
+    //todo call this to dispose of all sound assets loaded
+    SoundUtils.dispose();
   }
 }
